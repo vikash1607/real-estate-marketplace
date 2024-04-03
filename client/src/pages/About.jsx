5e1a0ff736c3e79c0af638dc093a74e88e92101a
Vikash Kumar <126924311+vikash1607@users.noreply.github.com>
2024-04-03 19:37:49 +0530

    Add files via upload

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Urban Estate</h1>
+      <p className='mb-4 text-slate-700'>Urban Estate isn't your average real estate agency. We're a leading company specializing in connecting you with the perfect property in the most desirable neighborhoods.  Our team of seasoned agents is passionate about providing exceptional service and making the buying, selling, or renting process as smooth and stress-free as possible.</p>
+      <p className='mb-4 text-slate-700'>At Urban Estate, we empower you to achieve your real estate dreams. Our expert agents leverage local market knowledge to provide personalized service, guiding you seamlessly through buying, selling, or renting your perfect property.</p>
+      <p className='mb-4 text-slate-700'>Urban Estate boasts a team of seasoned agents brimming with industry expertise. We prioritize exceptional service, ensuring your buying or selling journey is not just smooth, but an exciting and rewarding experience we're dedicated to creating for every client.</p>
+    </div>
+  )
+}
