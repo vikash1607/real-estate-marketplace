5593860e9d5b54772d608340fca88180ad0c8ff9
Vikash <2105083@kiit.ac.in>
2024-07-30 00:50:53 +0530

    last commit

---
+++ api/index.js
@@ -0,0 +1,68 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
+
+// mongoose.connect("mongodb://127.0.0.1:27017/mern-estate")
+mongoose.connect("mongodb+srv://vikash:vikash1@mern-estate.cxlcg29.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate")
+const db = mongoose.connection;
+
+// Define event listeners for database connection
+db.on('connected', () => {
+    console.log('Connected to MongoDB server');
+});
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+// mongoose
+//   .connect(process.env.MONGO)
+//   .then(() => {
+//     console.log('Connected to MongoDB!');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+
